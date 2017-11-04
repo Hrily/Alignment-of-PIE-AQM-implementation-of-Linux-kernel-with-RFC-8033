b0455af454a2857434217747b9ecd3dfbaa7845e
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2017-11-05 02:07:43 +0530

    Implemented Enabling/Disabling of PIE in alignment with RFC

--- net/sched/sch_pie.c
+++ net/sched/sch_pie.c
@@ -57,6 +57,7 @@ struct pie_vars {
 	psched_time_t dq_tstamp;	/* drain rate */
 	u32 avg_dq_rate;	/* bytes per pschedtime tick,scaled */
 	u32 qlen_old;		/* in bytes */
+	bool active; 		/* active/inactive */
 };
 
 /* statistics gathering */
@@ -93,6 +94,7 @@ static void pie_vars_init(struct pie_vars *vars)
 	vars->avg_dq_rate = 0;
 	/* default of 150 ms in pschedtime */
 	vars->burst_time = PSCHED_NS2TICKS(150 * NSEC_PER_MSEC);
+	vars->active = true;
 }
 
 static bool drop_early(struct Qdisc *sch, u32 packet_size)
@@ -140,12 +142,29 @@ static int pie_qdisc_enqueue(struct sk_buff *skb, struct Qdisc *sch,
 	struct pie_sched_data *q = qdisc_priv(sch);
 	bool enqueue = false;
 
+	if(!p->vars.active && 
+			qdisc_qlen(sch) >= sch->limit / 3 ) {
+		/* If queue is over certain threshold,
+		 * turn on PIE.
+		 */
+		pie_vars_init(&q->vars);
+		p->vars.qdelay_old = 0;
+		p->vars.prob = 0;
+		p->vars.dq_count = 0;
+		p->vars.dq_tstamp = psched_get_time();
+	}
+
 	if (unlikely(qdisc_qlen(sch) >= sch->limit)) {
 		q->stats.overlimit++;
 		goto out;
 	}
 
-	if (!drop_early(sch, skb->len)) {
+	if(!q->vars.active) {
+		/* If PIE is inactive
+		 * do not drop
+		 */
+		enqueue = true;
+	} else if (!drop_early(sch, skb->len)) {
 		enqueue = true;
 	} else if (q->params.ecn && (q->vars.prob <= MAX_PROB / 10) &&
 		   INET_ECN_set_ce(skb)) {
@@ -442,8 +461,11 @@ static void calculate_probability(struct Qdisc *sch)
 	if ((q->vars.qdelay < q->params.target / 2) &&
 	    (q->vars.qdelay_old < q->params.target / 2) &&
 	    (q->vars.prob == 0) &&
-	    (q->vars.avg_dq_rate > 0))
+	    (q->vars.avg_dq_rate > 0)) {
 		pie_vars_init(&q->vars);
+		/* Turn off PIE */
+		q->vars.active = false;
+	}
 }
 
 static void pie_timer(unsigned long arg)
@@ -453,7 +475,8 @@ static void pie_timer(unsigned long arg)
 	spinlock_t *root_lock = qdisc_lock(qdisc_root_sleeping(sch));
 
 	spin_lock(root_lock);
-	calculate_probability(sch);
+	if(q->vars.active)
+		calculate_probability(sch);
 
 	/* reset the timer to fire after 'tupdate'. tupdate is in jiffies. */
 	if (q->params.tupdate)
